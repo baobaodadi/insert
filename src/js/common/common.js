beaed61e19cc1e9c6e91854bbdab0ff779f83ba9
shendadi <shendadi@sogou-inc.com>
2018-09-21 23:51:27 +0800

    nb

--- src/js/common/common.js
+++ src/js/common/common.js
@@ -1,1 +1,1 @@
-import 'babel-polyfill';import 'bootstrap';import 'bootstrap-validator'const is_IE = function (ver) {  const b = document.createElement('b');  b.innerHTML = '<!--[if IE ' + ver + ']><i></i><![endif]-->';  return b.getElementsByTagName('i').length === 1;};if (is_IE(5) || is_IE(6) || is_IE(7) || is_IE(8)||is_IE(9)) {  window.location.href='/browser.html';}
+import 'babel-polyfill';import 'bootstrap';import './validator'
